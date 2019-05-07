1a859908f893fdeec19c7e3b68568b2a18245146
劉宸宗 <1101105310@gm.kuas.edu.tw>
2019-05-07 22:47:28 +0800

    Add feature: enable wifi at the beginning

--- app/src/main/java/tw/edu/ntut/sdtlab/crawler/testapp/testwifi/MainActivity.java
+++ app/src/main/java/tw/edu/ntut/sdtlab/crawler/testapp/testwifi/MainActivity.java
@@ -13,10 +13,15 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WifiManager wifiManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiManager.setWifiEnabled(true);
     }
 
     @Override
