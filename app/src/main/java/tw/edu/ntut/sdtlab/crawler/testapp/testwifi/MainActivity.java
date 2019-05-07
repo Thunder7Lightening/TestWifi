3893ad3ff7cc0e4f3d10765e4915706b7a67a353
劉宸宗 <1101105310@gm.kuas.edu.tw>
2019-05-07 22:40:52 +0800

    Initial commit
      - Only one page
      - If wifi is disabled at any time, this app will crash

---
+++ app/src/main/java/tw/edu/ntut/sdtlab/crawler/testapp/testwifi/MainActivity.java
@@ -0,0 +1,54 @@
+package tw.edu.ntut.sdtlab.crawler.testapp.testwifi;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        registerReceiver(wifiStateReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(wifiStateReceiver);
+    }
+
+    private BroadcastReceiver wifiStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int wifiStateExtra = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN);
+
+            switch (wifiStateExtra) {
+                case WifiManager.WIFI_STATE_ENABLED:
+                    // todo: nothing currently
+                    break;
+                case WifiManager.WIFI_STATE_DISABLED:
+                    crash();
+                    break;
+            }
+        }
+    };
+
+    private void crash() {
+        int i = 1 / 0;
+    }
+}
